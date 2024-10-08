d372f61e5d55c4155d54d9084803c891381a758a
Mark McConnell <McConnell-M22@ulster.ac.uk>
2024-10-08 10:09:50 +0100

    Initial commits

--- Library/Program.cs
+++ Library/Program.cs
@@ -1,2 +1,48 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Book
+{
+    string Title;
+    string Author;
+    string ISBN;
+    int NoOfPages;
+
+    void DisplayInfo()
+    {
+        // output book information to console
+        Console.WriteLine("Book information");
+        Console.WriteLine("----------------");
+        Console.WriteLine($"Book title: {Title}");
+        Console.WriteLine($"Book author: {Author}");
+        Console.WriteLine($"Book ISBN: {ISBN}");
+        Console.WriteLine($"No of Pages: {NoOfPages}");
+        Console.WriteLine("----------------");
+        Console.WriteLine();
+
+    }
+
+    static void Main(string[] args)
+    {
+        // create a new instance of the book class 
+        Book book = new Book();
+        book.Title = "C# for beginners";
+        book.Author = "Bill Gates";
+        book.ISBN = "556453006";
+        book.NoOfPages = 200;
+        
+        
+
+        Book book2 = new Book();
+        book2.Title = "C# Methods and Classes";
+        book2.Author = "Microsoft";
+        book2.ISBN = "76251303";
+        book2.NoOfPages = 50;
+
+        // output book information to the console 
+        book.DisplayInfo();
+        book2.DisplayInfo();
+
+       
+
+
+    }
+
+}
